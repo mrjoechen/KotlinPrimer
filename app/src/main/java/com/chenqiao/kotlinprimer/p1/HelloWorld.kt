c0332733dac60ac3b9c565e05bb964c1bfac867a
chenqiao <mrjoechen@qq.com>
2019-03-18 18:56:15 +0800

    initial commit

--- app/src/main/java/com/chenqiao/kotlinprimer/p1/HelloWorld.kt
+++ app/src/main/java/com/chenqiao/kotlinprimer/p1/HelloWorld.kt
@@ -6,5 +6,28 @@ package com.chenqiao.kotlinprimer.p1
  */
 
 
-var age = 18
-var name = "chen qiao"
+//变量 variable
+var age = 18 //自动推断
+var age1: Int = 18
+
+
+
+//不可变量 value
+val name = "chen qiao"
+
+val name1: String = "chenqiao"
+
+//val name2: String = null
+val name2: String? = null
+
+
+fun main(args: Array<String>) {
+    function("")
+}
+
+fun function(s: String) {
+    //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    print("hello")
+}
+
+
---
+++ app/src/main/java/com/chenqiao/kotlinprimer/p1/Test.java
@@ -0,0 +1,14 @@
+package com.chenqiao.kotlinprimer.p1;
+
+/**
+ * Created by chenqiao on 2019/3/14.
+ * e-mail : mrjctech@gmail.com
+ */
+public class Test {
+
+    public void echo(String s){
+        System.out.print(s);
+    }
+
+
+}
